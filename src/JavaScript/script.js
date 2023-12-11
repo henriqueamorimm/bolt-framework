dd38506b5606d6cb8cd7d4971275e17da8b77fb6
Aligg <114702742+henriqueamorimm@users.noreply.github.com>
2023-12-10 22:27:34 -0300

    Delete src directory

--- src/JavaScript/script.js
+++
@@ -1,9 +0,0 @@
-// Insert your JavaScript code´s here (will apply in all pages.)
-
-const JavaScript = `
-function showText(message){
-    console.log(message);
-  }
-  
-  `;
-module.exports = JavaScript;
