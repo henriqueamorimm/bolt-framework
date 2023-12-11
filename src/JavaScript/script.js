6f2ed6cd2c129717b039f43124f616d810080e92
Aligg <114702742+henriqueamorimm@users.noreply.github.com>
2023-12-10 22:27:54 -0300

    Add files via upload

---
+++ src/JavaScript/script.js
@@ -0,0 +1,9 @@
+// Insert your JavaScript code´s here (will apply in all pages.)
+
+const JavaScript = `
+function showText(message){
+    console.log(message);
+  }
+  
+  `;
+module.exports = JavaScript;
